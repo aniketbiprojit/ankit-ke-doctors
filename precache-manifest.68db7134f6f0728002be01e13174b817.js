01d30048e0c235d8452ab0aadc2c434a139a7b94
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-29 16:16:43 +0530

    Updates

---
+++ precache-manifest.68db7134f6f0728002be01e13174b817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8307ad8209713a962587b6225a81cd9f",
+    "url": "/ankit-ke-doctors/index.html"
+  },
+  {
+    "revision": "b93b296864768729d854",
+    "url": "/ankit-ke-doctors/static/css/main.67c41416.chunk.css"
+  },
+  {
+    "revision": "733baa561230eca22b2e",
+    "url": "/ankit-ke-doctors/static/js/2.87820a2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ankit-ke-doctors/static/js/2.87820a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93b296864768729d854",
+    "url": "/ankit-ke-doctors/static/js/main.14a0147c.chunk.js"
+  },
+  {
+    "revision": "0b666f6ba384ad922092",
+    "url": "/ankit-ke-doctors/static/js/runtime-main.10174a41.js"
+  }
+]);
