46e340740eebffd2e1e8160693d83a1c289a8475
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-29 18:47:48 +0530

    Content

--- client/src/scripts/Home/More.jsx
+++ client/src/scripts/Home/More.jsx
@@ -13,11 +13,24 @@ export default class More extends Component {
 									<img src={graphic} alt='' />
 									<p>Hello</p>
 								</div>
+								<div className='more-desc'>
+									<p>
+										Jab bhi kabhi <br />
+										Meri nazar
+									</p>
+								</div>
 							</div>
 							<div className='more-item'>
 								<div className='more-image'>
 									<img src={graphic} alt='' />
 									<p>Hello</p>
+								</div>
+								<div className='more-desc'>
+									<p>
+										Papa mujhe
+										<br />
+										Dhoondhe Tujhe
+									</p>
 								</div>
 							</div>
 						</div>
@@ -27,38 +40,58 @@ export default class More extends Component {
 									<img src={graphic} alt='' />
 									<p>Hello</p>
 								</div>
+								<div className='more-desc'>
+									<p>Yun zor se <br/> Ain na maa</p>
+								</div>
 							</div>
 							<div className='more-item'>
 								<div className='more-image'>
 									<img src={graphic} alt='' />
 									<p>Hello</p>
 								</div>
+								<div className='more-desc'>
+									<p>
+										Jhula Jhulate hai
+										<br />
+										Yun na rula
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div className='inner flex'>
-						<div className='more-item'>
-							<div className='more-image'>
-								<img src={graphic} alt='' />
-								<p>Hello</p>
+						<div className='flex'>
+							<div className='more-item'>
+								<div className='more-image'>
+									<img src={graphic} alt='' />
+									<p>Hello</p>
+								</div>
+								<div className='more-desc'>Jab bhi kabhi</div>
+							</div>
+							<div className='more-item'>
+								<div className='more-image'>
+									<img src={graphic} alt='' />
+									<p>Hello</p>
+								</div>
+								<div className='more-desc'>Papa mujhe</div>
 							</div>
 						</div>
-						<div className='more-item'>
-							<div className='more-image'>
-								<img src={graphic} alt='' />
-								<p>Hello</p>
+						<div className='flex'>
+							<div className='more-item'>
+								<div className='more-image'>
+									<img src={graphic} alt='' />
+									<p>Hello</p>
+								</div>
+								<div className='more-desc'>Yun zor se</div>
 							</div>
-						</div>
-						<div className='more-item'>
-							<div className='more-image'>
-								<img src={graphic} alt='' />
-								<p>Hello</p>
-							</div>
-						</div>
-						<div className='more-item'>
-							<div className='more-image'>
-								<img src={graphic} alt='' />
-								<p>Hello</p>
+							<div className='more-item'>
+								<div className='more-image'>
+									<img src={graphic} alt='' />
+									<p>Hello</p>
+								</div>
+								<div className='more-desc'>
+									Jhula Jhulate hai
+								</div>
 							</div>
 						</div>
 					</div>
